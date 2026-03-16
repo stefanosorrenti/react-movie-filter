df9410eb430e2632d365f7aa9698de9175f494d3
stefanosorrenti <stefanosorrenti01@gmail.com>
2026-03-16 11:35:05 +0100

    add AppMain component

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,6 @@
 //IMPORT
 import AppHeader from "./components/AppHeader"
+import AppMain from "./components/AppMain"
 
 
 function App() {
@@ -8,6 +9,7 @@ function App() {
   return (
     <> 
       <AppHeader />
+      <AppMain />
     </>
   )
 }
---
+++ src/components/AppMain.jsx
@@ -0,0 +1,10 @@
+export default function AppMain () {
+
+    return(
+
+        /* Main */
+        <main>
+           
+        </main>
+    )
+}
