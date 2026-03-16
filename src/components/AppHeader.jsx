44d869425bbdb6e1fc6d200f70c44e182b3861d5
stefanosorrenti <stefanosorrenti01@gmail.com>
2026-03-16 18:05:25 +0100

    AppHeader: add css

--- src/components/AppHeader.jsx
+++ src/components/AppHeader.jsx
@@ -1,11 +1,13 @@
-export default function AppHeader ()  {
+export default function AppHeader() {
 
 
     return (
 
         /* Header */
-        <header>
-            <h1>I miei film.</h1>
+        <header className="text-center fs-1 p-3">
+            <div className="container">
+                <h1>I MIEI FILM.</h1>
+            </div>
         </header>
     )
 }
--- src/main.jsx
+++ src/main.jsx
@@ -1,5 +1,7 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
+// Supports weights 300-900
+import '@fontsource-variable/merriweather';
 import './index.css'
 import App from './App.jsx'
 
